75204c0786675a9fe50d73203483b9d3bd726733
richo4r.tgz@gmail.com <richo4r.tgz@gmail.com>
2021-12-20 15:24:13 +0700

    init

---
+++ Website/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebPerusahaan.Models;
+
+namespace WebPerusahaan.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public AccountController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
